5a3c4047ac2d1fe40396830c771000a50ef25473
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2022-08-23 20:41:20 +0530

    added DataManagementProgram

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace DataInventoryManagment
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            string Path = "C:\\Users\\Guest 1\\Desktop\\New folder\\DataInventoryManagment\\DataInventoryManagment\\FetchJsonForRice.cs ";
+            FetchJsonForRice fetchJsonForRice = new FetchJsonForRice();
+
+            Rice data = fetchJsonForRice.Read(Path);
+
+            Console.WriteLine(data.typesOfRice.name);
+            Console.WriteLine(data.typesOfRice.price);
+            Console.WriteLine(data.typesOfRice.weight);
+
+            Console.WriteLine("-----------------------------------");
+
+           
+            Console.WriteLine(data.typesOfWheat.name);
+            Console.WriteLine(data.typesOfWheat.price);
+            Console.WriteLine(data.typesOfWheat.weight);
+
+            Console.WriteLine("-----------------------------------");
+
+            
+            Console.WriteLine(data.typesOfPulse.name);
+            Console.WriteLine(data.typesOfPulse.price);
+            Console.WriteLine(data.typesOfPulse.weight);
+
+            Console.WriteLine("-----------------------------------");
+
+
+
+        }
+    }
+}
---
+++ Rice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataInventoryManagment
+{
+    public class Rice
+    {
+        public TypesOfRice typesOfRice;
+        public TypesOfPulse typesOfPulse;
+        public TypesOfWheat typesOfWheat;
+    }
+    public class TypesOfRice
+    {
+        public int name;
+        public int weight;
+        public int price;
+    }
+    public class TypesOfPulse
+    {
+        public int name;
+        public int weight;
+        public int price;
+    }
+    public class TypesOfWheat
+    {
+        public int name;
+        public int weight;
+        public int price;
+    }
+}
